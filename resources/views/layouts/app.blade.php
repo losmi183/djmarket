fc891b701fae3620e7c86172590ec0e2a1bcdbf3
losmi183 <milos.glogovac@gmail.com>
2020-08-04 13:02:35 +0200

    Google AdSence added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,700;0,800;1,400&family=Roboto:wght@300;500;700;900&display=swap" rel="stylesheet">    
     
+    {{-- Google AdSence  --}}
+    <script data-ad-client="ca-pub-2402254232352226" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
